c965bec9703b7cbef96384549a89bcd25bb6d7c8
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2018-08-28 02:04:34 +0200

    [:hammer:] serve react from express server

--- wordux-api/server.js
+++ wordux-api/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import { resolve } from 'path';
 import apiRouter from './src/router';
 
 (() => {
   const app = express();
+
+  const appDistPath = resolve(__dirname, '..', 'wordux-app', 'dist');
+  app.use(express.static(appDistPath));
+
   app.use('/api', apiRouter);
+
   app.listen(3000, () => {
     console.info(`
       \\\\\\         ///
